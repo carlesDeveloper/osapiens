c7017c58a37ff21b1e05a66b3eccbc2ce719dcd0
carlesDeveloper <carles_blaugrana@hotmail.com>
2024-03-14 09:17:32 +0100

    minor improve

--- frontend_osapiens/src/pages/PlanetsPage.tsx
+++ frontend_osapiens/src/pages/PlanetsPage.tsx
@@ -4,13 +4,15 @@ import "../assets/css/planets.css"
 import Pagination from '../components/Pagination';
 
 function PlanetsPage() {
-    
+
 
     return (
         <>
-            <div className='planets__title'>Planets</div>
-            <PlanetsTable />
-            <Pagination />
+            <div className="planets__section">
+                <div className='planets__title'>Planets</div>
+                <PlanetsTable />
+                <Pagination />
+            </div>
         </>
     )
 }
